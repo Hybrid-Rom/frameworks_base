7e6206769e86408473bd6c5afb172f3f29533b6a
Lars Greiss <kufikugel@googlemail.com>
2013-11-08 20:28:53 +0100

    Volume rocker control (wake up)
    Change-Id: I60f5f88e32a129afda80f5b0f4f41cb0a322b691

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3084,6 +3084,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
