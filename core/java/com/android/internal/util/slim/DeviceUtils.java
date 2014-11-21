185a36823c2cc2245b930ba31b0796e907af2896
Lars Greiss <kufikugel@googlemail.com>
2014-11-21 17:42:55 +0100

    Frameworks: Add several helper classes and constants
    Thanks to Slim

--- core/java/com/android/internal/util/slim/DeviceUtils.java
+++ core/java/com/android/internal/util/slim/DeviceUtils.java
@@ -84,6 +84,11 @@ public class DeviceUtils {
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
     }
 
+    public static boolean deviceSupportsImeSwitcher(Context ctx) {
+        Resources res = ctx.getResources();
+        return res.getBoolean(com.android.internal.R.bool.config_show_IMESwitcher);
+    }
+
     public static boolean adbEnabled(ContentResolver resolver) {
             return (Settings.Global.getInt(resolver, Settings.Global.ADB_ENABLED, 0)) == 1;
     }
@@ -122,12 +127,14 @@ public class DeviceUtils {
     }
 
     private static boolean isSupportedFeature(Context context, String action) {
-        if (action.equals(ActionConstants.ACTION_TORCH)
+        if (action.equals(SlimActionConstants.ACTION_TORCH)
                         && !deviceSupportsTorch(context)
-                || action.equals(ActionConstants.ACTION_VIB)
+                || action.equals(SlimActionConstants.ACTION_VIB)
                         && !deviceSupportsVibrator(context)
-                || action.equals(ActionConstants.ACTION_VIB_SILENT)
-                        && !deviceSupportsVibrator(context)) {
+                || action.equals(SlimActionConstants.ACTION_VIB_SILENT)
+                        && !deviceSupportsVibrator(context)
+                || action.equals(SlimActionConstants.ACTION_SMART_PULLDOWN)
+                        && isTablet(context)) {
             return false;
         }
         return true;
