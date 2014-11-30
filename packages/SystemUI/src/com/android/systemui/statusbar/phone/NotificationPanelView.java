4e1e71653db3cd1d82e21dfea6bed7ecf9266cd7
Lars Greiss <kufikugel@googlemail.com>
2014-11-30 21:55:26 +0100

    Frameworks: add abillity to use quick settings pulldown on the left side
    This commit gives the ability to use quick settings pulldown on the left
    or right edge of the statusbar. As well this respects still RTL layouts
    where it logically should change to the oposite side.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -966,7 +966,7 @@ public class NotificationPanelView extends PanelView implements
             return true;
         }
     };
-    
+
     private void animateHeaderSlidingIn() {
         mHeaderAnimatingIn = true;
         getViewTreeObserver().addOnPreDrawListener(mStartHeaderSlidingIn);
