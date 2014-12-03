6246bac1f398c8b1f89da08ca3fa08fb62a29e2c
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 19:56:41 +0100

    Return: Smart Pulldown [1/2]
    Adds the ability to directly open the QS panel when there are
    no notifications present in the notification panel.
    This commit combines the enhancement by HardCorePawn for detectin
    notification types for the same.
    
    Credits: kufikugel, HardCorePawn
    
    PS 2-4 : Cleanup
    
    Adapted to LP
    
    Added Jubakubas commit
    
    SystemUI: Smart Pull-Down - Any Notification (1/2)
    
    PS
    - fixed logic to go inline with all mode in settings - should be fixed on kk as well
    
    Change-Id: I3c9b048f2616d2b8950930cc5bfac27c2364e826

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1844,6 +1844,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    protected boolean hasActiveVisibleNotifications() {
+        return mNotificationData.hasActiveVisibleNotifications();
+    }
+
+    protected boolean hasActiveClearableNotifications() {
+        return mNotificationData.hasActiveClearableNotifications();
+    }
+
     @Override
     protected void setAreThereNotifications() {
 
