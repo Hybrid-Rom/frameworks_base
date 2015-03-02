9abaeefabb433df429da737989619514710c48d2
k5t4j5 <boogs19.kj@gmail.com>
2015-03-02 15:19:37 +0000

    Support GESTURE_SENSOR input device type with GestureService
    * The GESTURE_SENSOR input device type is meant to support touch sensors
      which are meant for gesture input only, very similar to a touchpad,
      but without the pointer capability.
    * Define separate service to handle gestures from GESTURE_SENSOR device
      type.
    
    Change-Id: I9b273df2a3cc141774d7f7cd81e43a90ea5b230b
    
    Hide InputDevice.SOURCE_GESTURE_SENSOR from API
    
    Change-Id: If009e9595fc593594b0e7764669996de137483a1
    
    GestureInput : Allow doubletap/longpress configuration
    
    Allows devices to specify pending intents for double tap and
    long press events.
    
    Change-Id: I7e7cc2f9f96a01d8f6232e5cf0e19832fdfd5359
    
    Conflicts:
    	Android.mk
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/SystemServer.java

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -73,6 +73,7 @@ import com.android.server.display.DisplayManagerService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.fingerprint.FingerprintService;
 import com.android.server.hdmi.HdmiControlService;
+import com.android.server.gesture.GestureService;
 import com.android.server.input.InputManagerService;
 import com.android.server.job.JobSchedulerService;
 import com.android.server.lights.LightsManager;
@@ -532,6 +533,7 @@ public final class SystemServer {
         AssetAtlasService atlas = null;
         MediaRouterService mediaRouter = null;
         ThemeService themeService = null;
+        GestureService gestureService = null;
 
         // Bring up services needed for UI.
         if (mFactoryTestMode != FactoryTest.FACTORY_TEST_LOW_LEVEL) {
@@ -939,6 +941,17 @@ public final class SystemServer {
                 }
             }
 
+            if (context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_enableGestureService)) {
+                try {
+                    Slog.i(TAG, "Gesture Sensor Service");
+                    gestureService = new GestureService(context, inputManager);
+                    ServiceManager.addService("gesture", gestureService);
+                } catch (Throwable e) {
+                    Slog.e(TAG, "Failure starting Gesture Sensor Service", e);
+                }
+            }
+
             if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_PRINTING)) {
                 mSystemServiceManager.startService(PRINT_MANAGER_SERVICE_CLASS);
             }
@@ -1098,6 +1111,14 @@ public final class SystemServer {
         filter.addCategory(Intent.CATEGORY_THEME_PACKAGE_INSTALLED_STATE_CHANGE);
         filter.addDataScheme("package");
         context.registerReceiver(new AppsLaunchFailureReceiver(), filter);
+
+        if (gestureService != null) {
+            try {
+                gestureService.systemReady();
+            } catch (Throwable e) {
+                reportWtf("making Gesture Sensor Service ready", e);
+            }
+        }
 
         // These are needed to propagate to the runnable below.
         final MountService mountServiceF = mountService;
